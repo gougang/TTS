40f680310225351c4f80c269b3252a981f9e1e54
苟刚 <gang.gou@ly.com>
2024-02-28 10:30:37 +0800

    feat: 提交最新训练数据

--- TTS/demos/xtts_ft_demo/xtts_demo.py
+++ TTS/demos/xtts_ft_demo/xtts_demo.py
@@ -18,13 +18,17 @@ from TTS.demos.xtts_ft_demo.utils.gpt_train import train_gpt
 from TTS.tts.configs.xtts_config import XttsConfig
 from TTS.tts.models.xtts import Xtts
 
+
 # export PYTHONPATH=/root/github/TTS:$PYTHONPATH
 def clear_gpu_cache():
     # clear the GPU cache
     if torch.cuda.is_available():
         torch.cuda.empty_cache()
 
+
 XTTS_MODEL = None
+
+
 def load_model(xtts_checkpoint, xtts_config, xtts_vocab):
     global XTTS_MODEL
     clear_gpu_cache()
@@ -34,24 +38,29 @@ def load_model(xtts_checkpoint, xtts_config, xtts_vocab):
     config.load_json(xtts_config)
     XTTS_MODEL = Xtts.init_from_config(config)
     print("Loading XTTS model! ")
-    XTTS_MODEL.load_checkpoint(config, speaker_file_path='/root/model/XTTS-v2/speakers_xtts.pth', checkpoint_path=xtts_checkpoint, vocab_path=xtts_vocab, use_deepspeed=False)
+    XTTS_MODEL.load_checkpoint(config, speaker_file_path='/root/model/XTTS-v2/speakers_xtts.pth',
+                               checkpoint_path=xtts_checkpoint, vocab_path=xtts_vocab, use_deepspeed=False)
     if torch.cuda.is_available():
         XTTS_MODEL.cuda()
 
     print("Model Loaded!")
     return "Model Loaded!"
 
+
 def run_tts(lang, tts_text, speaker_audio_file):
     if XTTS_MODEL is None or not speaker_audio_file:
         return "You need to run the previous step to load the model !!", None, None
 
-    gpt_cond_latent, speaker_embedding = XTTS_MODEL.get_conditioning_latents(audio_path=speaker_audio_file, gpt_cond_len=XTTS_MODEL.config.gpt_cond_len, max_ref_length=XTTS_MODEL.config.max_ref_len, sound_norm_refs=XTTS_MODEL.config.sound_norm_refs)
+    gpt_cond_latent, speaker_embedding = XTTS_MODEL.get_conditioning_latents(audio_path=speaker_audio_file,
+                                                                             gpt_cond_len=XTTS_MODEL.config.gpt_cond_len,
+                                                                             max_ref_length=XTTS_MODEL.config.max_ref_len,
+                                                                             sound_norm_refs=XTTS_MODEL.config.sound_norm_refs)
     out = XTTS_MODEL.inference(
         text=tts_text,
         language=lang,
         gpt_cond_latent=gpt_cond_latent,
         speaker_embedding=speaker_embedding,
-        temperature=XTTS_MODEL.config.temperature, # Add custom parameters here
+        temperature=XTTS_MODEL.config.temperature,  # Add custom parameters here
         length_penalty=XTTS_MODEL.config.length_penalty,
         repetition_penalty=XTTS_MODEL.config.repetition_penalty,
         top_k=XTTS_MODEL.config.top_k,
@@ -66,9 +75,7 @@ def run_tts(lang, tts_text, speaker_audio_file):
     return "Speech generated !", out_path, speaker_audio_file
 
 
-
-
-# define a logger to redirect 
+# define a logger to redirect
 class Logger:
     def __init__(self, filename="log.out"):
         self.log_file = filename
@@ -86,13 +93,14 @@ class Logger:
     def isatty(self):
         return False
 
+
 # redirect stdout and stderr to a file
 sys.stdout = Logger()
 sys.stderr = sys.stdout
 
-
 # logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 import logging
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
@@ -100,6 +108,7 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)
     ]
 )
+
 
 def read_logs():
     sys.stdout.flush()
@@ -113,13 +122,13 @@ if __name__ == "__main__":
     current_datetime = datetime.now()
     # 格式化日期时间为字符串
     formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
-    
+
     parser = argparse.ArgumentParser(
         description="""XTTS fine-tuning demo\n\n"""
-        """
-        Example runs:
-        python3 TTS/demos/xtts_ft_demo/xtts_demo.py --port 
-        """,
+                    """
+                    Example runs:
+                    python3 TTS/demos/xtts_ft_demo/xtts_demo.py --port 
+                    """,
         formatter_class=argparse.RawTextHelpFormatter,
     )
     parser.add_argument(
@@ -132,7 +141,7 @@ if __name__ == "__main__":
         "--out_path",
         type=str,
         help="Output path (where data and checkpoints will be saved) Default: /tmp/xtts_ft/",
-        default="/root/xtts_ft/"+formatted_datetime,
+        default="/root/xtts_ft/" + formatted_datetime,
     )
 
     parser.add_argument(
@@ -209,43 +218,65 @@ if __name__ == "__main__":
             demo.load(read_logs, None, logs, every=1)
 
             prompt_compute_btn = gr.Button(value="Step 1 - Create dataset")
-        
+
+
             def preprocess_dataset(audio_path, language, out_path, progress=gr.Progress(track_tqdm=True)):
                 clear_gpu_cache()
                 out_path = os.path.join(out_path, "dataset")
                 os.makedirs(out_path, exist_ok=True)
                 if audio_path is None:
                     audio_path = [
-                    "/root/static/chenghong/10_10_chenghong_00009_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/11_11_chenghong_00010_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/12_12_chenghong_00011_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/13_13_chenghong_00012_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/14_14_chenghong_00013_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/15_15_chenghong_00014_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/16_16_chenghong_00015_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/17_17_chenghong_00016_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/19_19_chenghong_00018_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/1_1_chenghong_00000_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/20_20_chenghong_00019_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/21_21_chenghong_00020_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/22_22_chenghong_00021_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/24_24_chenghong_00023_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/25_25_chenghong_00024_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/27_27_chenghong_00026_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/2_2_chenghong_00001_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/3_3_chenghong_00002_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/4_4_chenghong_00003_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/5_5_chenghong_00004_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/6_6_chenghong_00005_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/7_7_chenghong_00006_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/8_8_chenghong_00007_(Vocals)_(Vocals).mp3",
-                    "/root/static/chenghong/9_9_chenghong_00008_(Vocals)_(Vocals).mp3"
+                        "/home/hotel/xtts/training-data/gl/10_33_gaoling_00032_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/11_32_gaoling_00031_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/12_31_gaoling_00030_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/13_30_gaoling_00029_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/14_29_gaoling_00028_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/1_42_gaoling_00041_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/15_28_gaoling_00027_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/16_27_gaoling_00026_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/17_26_gaoling_00025_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/18_25_gaoling_00024_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/19_24_gaoling_00023_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/20_23_gaoling_00022_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/21_22_gaoling_00021_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/22_21_gaoling_00020_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/23_20_gaoling_00019_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/24_19_gaoling_00018_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/2_41_gaoling_00040_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/25_18_gaoling_00017_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/26_17_gaoling_00016_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/27_16_gaoling_00015_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/28_15_gaoling_00014_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/29_14_gaoling_00013_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/30_13_gaoling_00012_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/31_12_gaoling_00011_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/32_11_gaoling_00010_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/33_10_gaoling_00009_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/3_40_gaoling_00039_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/34_9_gaoling_00008_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/35_8_gaoling_00007_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/36_7_gaoling_00006_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/37_6_gaoling_00005_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/38_5_gaoling_00004_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/39_4_gaoling_00003_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/40_3_gaoling_00002_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/41_2_gaoling_00001_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/42_1_gaoling_00000_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/4_39_gaoling_00038_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/5_38_gaoling_00037_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/6_37_gaoling_00036_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/7_36_gaoling_00035_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/8_35_gaoling_00034_(Vocals)_(Vocals).mp3",
+                        "/home/hotel/xtts/training-data/gl/9_34_gaoling_00033_(Vocals)_(Vocals).mp3"
                     ]
                 if audio_path is None:
                     return "You should provide one or multiple audio files! If you provided it, probably the upload of the files is not finished yet!", "", ""
                 else:
                     try:
-                        train_meta, eval_meta, audio_total_size = format_audio_list(audio_path, target_language=language, out_path=out_path, gradio_progress=progress)
+                        train_meta, eval_meta, audio_total_size = format_audio_list(audio_path,
+                                                                                    target_language=language,
+                                                                                    out_path=out_path,
+                                                                                    gradio_progress=progress)
                     except:
                         traceback.print_exc()
                         error = traceback.format_exc()
@@ -269,7 +300,7 @@ if __name__ == "__main__":
             eval_csv = gr.Textbox(
                 label="Eval CSV:",
             )
-            num_epochs =  gr.Slider(
+            num_epochs = gr.Slider(
                 label="Number of epochs:",
                 minimum=1,
                 maximum=100,
@@ -307,14 +338,23 @@ if __name__ == "__main__":
             demo.load(read_logs, None, logs_tts_train, every=1)
             train_btn = gr.Button(value="Step 2 - Run the training")
 
-            def train_model(language, train_csv, eval_csv, num_epochs, batch_size, grad_acumm, output_path, max_audio_length):
+
+            def train_model(language, train_csv, eval_csv, num_epochs, batch_size, grad_acumm, output_path,
+                            max_audio_length):
                 clear_gpu_cache()
                 if not train_csv or not eval_csv:
                     return "You need to run the data processing step or manually set `Train CSV` and `Eval CSV` fields !", "", "", "", ""
                 try:
                     # convert seconds to waveform frames
                     max_audio_length = int(max_audio_length * 22050)
-                    config_path, original_xtts_checkpoint, vocab_file, exp_path, speaker_wav = train_gpt(language, num_epochs, batch_size, grad_acumm, train_csv, eval_csv, output_path=output_path, max_audio_length=max_audio_length)
+                    config_path, original_xtts_checkpoint, vocab_file, exp_path, speaker_wav = train_gpt(language,
+                                                                                                         num_epochs,
+                                                                                                         batch_size,
+                                                                                                         grad_acumm,
+                                                                                                         train_csv,
+                                                                                                         eval_csv,
+                                                                                                         output_path=output_path,
+                                                                                                         max_audio_length=max_audio_length)
                 except:
                     traceback.print_exc()
                     error = traceback.format_exc()
@@ -404,7 +444,6 @@ if __name__ == "__main__":
                 ],
             )
 
-
             train_btn.click(
                 fn=train_model,
                 inputs=[
@@ -419,7 +458,7 @@ if __name__ == "__main__":
                 ],
                 outputs=[progress_train, xtts_config, xtts_vocab, xtts_checkpoint, speaker_reference_audio],
             )
-            
+
             load_btn.click(
                 fn=load_model,
                 inputs=[
@@ -443,6 +482,7 @@ if __name__ == "__main__":
     demo.launch(
         share=True,
         debug=False,
-        server_port= 6006,
+        server_port=8808,
+        root_path="/jupyter-port/1121271/8808/xtts_ft_demo",
         server_name="0.0.0.0"
     )
